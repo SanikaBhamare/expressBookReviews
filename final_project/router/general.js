00b72ec2b878e69f038256f3be3147e23784e94d
Sanika Bhamare <sanikabhamare@gmail.com>
2024-10-01 15:17:47 -0700

    get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,18 +25,36 @@ public_users.get("/isbn/:isbn", function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {});
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+
+  let booksByAuthor = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase()
+  );
+
+  if (booksByAuthor.length > 0) {
+    res.send(JSON.stringify(booksByAuthor));
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+
+  let booksByTitle = Object.values(books).filter(
+    (book) => book.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (booksByTitle.length > 0) {
+    res.send(JSON.stringify(booksByTitle));
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get("/review/:isbn", function (req, res) {});
 
 module.exports.general = public_users;
